539dd0f75f9eddc274fd8454929507ae7d674528
надежда <rinstren@mail.ru>
2022-07-12 22:29:58 +0300

    Очередная правка фунции подставки

--- js/form.js
+++ js/form.js
@@ -22,15 +22,13 @@ const errorTemplate = document.querySelector('#error').content.querySelector('.e
 const fileChooser = document.querySelector('.img-upload__input');
 
 const uploadImage = () => {
-  fileChooser.addEventListener('change', () => {
-    const file = fileChooser.files[0];
-    const fileName = file.name.toLowerCase();
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
 
-    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
-    if (matches) {
-      image.src = URL.createObjectURL(file);
-    }
-  });
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (matches) {
+    image.src = URL.createObjectURL(file);
+  }
 };
 
 //Функция открытия окна редактирования
